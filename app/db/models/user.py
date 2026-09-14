558e9c14767ad8b63208d7a64674502d65a6f14c
Shailesh <shaileshpandit141@gmail.com>
2026-09-14 22:01:26 +0530

    feat: recreate user table

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,27 +1,22 @@
-from datetime import datetime
-
-from sqlalchemy import DateTime, String, func
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base import Base
 
+from ._mixins import UUID7Mixin
 
-class User(Base):
+
+class User(UUID7Mixin, Base):
     __tablename__: str = "users"
 
-    id: Mapped[int] = mapped_column(
-        primary_key=True,
-        autoincrement=True,
-    )
-
-    username: Mapped[str] = mapped_column(
-        String(32),
-        unique=True,
+    email: Mapped[str] = mapped_column(
+        String(320),
         nullable=False,
+        unique=True,
+        index=True,
     )
 
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
+    password_hash: Mapped[str] = mapped_column(
+        String(255),
         nullable=False,
     )
